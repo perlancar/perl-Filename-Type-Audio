d4cd8a5660c2fc425bccc46e3897c7d6ffb1754d
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-10-20 16:23:25 +0700

    Add Rinci metadata, move tests to examples in function metadata

--- lib/Filename/Audio.pm
+++ lib/Filename/Audio.pm
@@ -16,6 +16,57 @@ our @EXPORT_OK = qw(check_audio_filename);
 our $STR_RE = "mpega|aifc|aiff|flac|midi|mpga|opus|aif|amr|awb|axa|csd|gsm|kar|m3u|m4a|mid|mp2|mp3|oga|ogg|orc|pls|ram|sco|sd2|sid|snd|spx|wav|wax|wma|au|ra|rm"; # STR_RE
 our $RE = qr(\.(?:$STR_RE)\z)i;
 
+our %SPEC;
+
+$SPEC{check_audio_filename} = {
+    v => 1.1,
+    summary => 'Check whether filename indicates being an audio file',
+    description => <<'_',
+
+
+_
+    args => {
+        filename => {
+            schema => 'filename*',
+            req => 1,
+            pos => 0,
+        },
+        # XXX recurse?
+        #ci => {
+        #    summary => 'Whether to match case-insensitively',
+        #    schema  => 'bool',
+        #    default => 1,
+        #},
+    },
+    result_naked => 1,
+    result => {
+        schema => ['any*', of=>['bool*', 'hash*']],
+        description => <<'_',
+
+Return false if no archive suffixes detected. Otherwise return a hash of
+information.
+
+_
+    },
+    examples => [
+        {
+            args => {filename => 'foo.txt'},
+            naked_result => 0,
+        },
+        {
+            args => {filename => 'foo.mp4'},
+            naked_result => 0,
+        },
+        {
+            args => {filename => 'foo.wav'},
+            naked_result => {},
+        },
+        {
+            args => {filename => 'foo.MP3'},
+            naked_result => {},
+        },
+    ],
+};
 sub check_audio_filename {
     my %args = @_;
 
@@ -23,7 +74,7 @@ sub check_audio_filename {
 }
 
 1;
-# ABSTRACT: Check whether filename indicates being an audio file
+# ABSTRACT:
 
 =head1 SYNOPSIS
 
@@ -39,11 +90,6 @@ sub check_audio_filename {
 =head1 DESCRIPTION
 
 
-=head1 FUNCTIONS
-
-=head2 check_audio_filename
-
-
 =head1 SEE ALSO
 
 L<Filename::Video>
@@ -53,5 +99,7 @@ L<Filename::Image>
 L<Filename::Ebook>
 
 L<Filename::Media>
+
+Other Filename::*
 
 =cut
